66599daa14b11ba405e0dda54e629becaecb79f8
Kevin <globalavocado@users.noreply.github.com>
2017-06-06 11:36:49 +0100

    fixed update errors

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -5,6 +5,7 @@ sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 sudo dpkg-reconfigure debconf -f noninteractive -p critical
 
 # install Ansible
+apt-get -y update
 apt-get -y install software-properties-common python-software-properties
 apt-add-repository ppa:ansible/ansible
 apt-get -y update
